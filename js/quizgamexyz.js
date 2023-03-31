5a27e719761e9ff47c9f1505b62c526e1b903ea9
Tejas <mtejas619@gmail.com>
2023-03-31 23:08:02 +0530

    updated

--- js/quizgamexyz.js
+++
@@ -1,9 +0,0 @@
-let person = prompt("Please enter your name", "Harry Potter");
-let text;
-if (person == null || person == "") {
-  text = "User cancelled the prompt.";
-}
-else {
-  text = `Hello ` + person + `! How are you today?`
-}
-prompt(text)
